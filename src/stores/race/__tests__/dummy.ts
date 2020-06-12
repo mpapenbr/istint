dbbad5f2654a659baf5ed0900857d93a559c12c6
mpapenbr <Markus.Papenbrock@gmail.com>
2020-06-12 20:38:40 +0200

    Mach was kaputt Fixes #38

--- src/stores/race/__tests__/dummy.ts
+++ src/stores/race/__tests__/dummy.ts
@@ -1,16 +1,16 @@
-import { equal } from "assert";
-
 describe("what does this?", () => {
+  test("1==1 should be true ", () => {
+    const a = 1;
+    expect(a).toEqual(1);
+  });
 
-    test("1==1 should be true ", () => {
-        const a = 1
-        expect(a).toEqual(1)
-    });
+  it("should detect other stuff", () => {
+    const a = 1;
+    expect(a).not.toEqual(2);
+  });
 
-    it("should detect other stuff", () => {
-        const a = 1
-        expect(a).not.toEqual(2)
-    });
-
-
+  it("this one will break by design", () => {
+    const a = 1;
+    expect(a).not.toEqual(1);
+  });
 });
