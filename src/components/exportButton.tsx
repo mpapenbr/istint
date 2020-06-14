e606f58de79b0b4ba657da6de24c2abc4d9f701e
mpapenbr <Markus.Papenbrock@gmail.com>
2020-06-14 12:03:43 +0200

    Export via dialog Fixes #46 Bonus: Non-prod-deps nach devDeps verschoben

--- src/components/exportButton.tsx
+++ src/components/exportButton.tsx
@@ -1,12 +1,13 @@
 import { Button, Modal } from "antd";
+import TextArea from "antd/lib/input/TextArea";
+import copy from "copy-to-clipboard";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { ApplicationState } from "../stores";
 import { compressJson } from "../utils/compressJson";
 
 const ExportButton: React.FC = () => {
-  const myAppState = useSelector(
-    (appState: ApplicationState) => (appState))
+  const myAppState = useSelector((appState: ApplicationState) => appState);
 
   const [visible, setVisible] = useState(false);
   const [urlData, setUrlData] = useState("");
@@ -14,22 +15,32 @@ const ExportButton: React.FC = () => {
   const handleClick = () => {
     setVisible(true);
     // console.log(myAppState)
-    compressJson(myAppState).then(d => setUrlData(d))
-  }
+    compressJson(myAppState).then((d) => setUrlData(d));
+  };
   const closeDialog = () => {
-    setVisible(false)
-  }
+    setVisible(false);
+  };
+
+  const copyAndCloseDialog = () => {
+    copy(urlData);
+    setVisible(false);
+  };
 
   // console.log(whereAmI)
   return (
     <>
-
       <Button onClick={handleClick}>Export</Button>
-      <Modal visible={visible} title="Data export" onCancel={closeDialog} onOk={closeDialog} >
-        {urlData}
+      <Modal
+        visible={visible}
+        title="Export data to clipboard"
+        onCancel={closeDialog}
+        onOk={copyAndCloseDialog}
+        okButtonProps={{ title: "copy data to clipboard" }}
+      >
+        <TextArea rows={10} value={urlData} readOnly={true} />
       </Modal>
     </>
   );
-}
+};
 
 export default ExportButton;
