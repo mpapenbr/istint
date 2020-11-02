2268971f712d1c25509f9e63f570fe3b2acdfef9
mpapenbr <Markus.Papenbrock@gmail.com>
2020-11-02 21:52:59 +0100

    Umstellung auf Test-Keycloak-Instanz

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,8 @@
 // export const API_BASE_URL = 'http://localhost:18080/irsdk';
 export const API_BASE_URL = process.env.REACT_APP_BASE_URL;
 export const API_AUTH_URL = process.env.REACT_APP_AUTH_URL;
+export const API_AUTH_REALM = process.env.REACT_APP_AUTH_REALM || "pleaseConfigureRealm";
+export const API_AUTH_CLIENT = process.env.REACT_APP_AUTH_CLIENT || "pleaseConfigureClient";
 export const ACCESS_TOKEN = "accessToken";
 export const EXT_LOAD_ID = "extLoadId";
 export const SERVER_KEY = "servers"; //
--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,12 +1,12 @@
 import Keycloak from "keycloak-js";
-import { API_AUTH_URL } from "./constants";
+import { API_AUTH_CLIENT, API_AUTH_REALM, API_AUTH_URL } from "./constants";
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
-  realm: "iracing-tools",
+  realm: API_AUTH_REALM,
   url: API_AUTH_URL,
-  clientId: "login-web",
+  clientId: API_AUTH_CLIENT,
 });
 
 export default keycloak;
