de3a4a877b5c9922e6779e327ddd2fb9b1c88495
mpapenbr <41362745+mpapenbr@users.noreply.github.com>
2020-11-16 21:58:11 +0100

    Mpapenbr/issue102 (#104)
    * Sortable eventlist columns
    Fixes #102
    
    * filterable event,car,track
    
    * highlighter for search filter, antd 4.8.4
    
    Co-authored-by: mpapenbr <Markus.Papenbrock@gmail.com>

--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -1,8 +1,10 @@
-import { CopyOutlined, DeleteOutlined, EditOutlined, ShareAltOutlined } from "@ant-design/icons";
-import { Button, notification, Table, Tooltip } from "antd";
+import { CopyOutlined, DeleteOutlined, EditOutlined, SearchOutlined, ShareAltOutlined } from "@ant-design/icons";
+import { Button, Input, notification, Space, Table, Tooltip } from "antd";
 import { ColumnsType } from "antd/lib/table";
+import { FilterDropdownProps } from "antd/lib/table/interface";
 import copy from "copy-to-clipboard";
-import React from "react";
+import React, { useState } from "react";
+import Highlighter from "react-highlight-words";
 import { v4 as uuidv4 } from "uuid";
 import { MyEvent } from "../../api/events";
 
@@ -72,28 +74,117 @@ const EventList: React.FC<MyProps> = (props: MyProps) => {
       </Tooltip>
     </div>
   );
+  const dateCompareValue = (d: Date | undefined | string) => {
+    if (d === undefined) return 0;
+    if (typeof d === "string") return new Date(d).getTime();
+    if (typeof d === "object") return (d as Date).getTime();
+    return 0;
+  };
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  var searchInput: any;
+  const getColumnSearchProps = (
+    dataIndex: any,
+    searchPlacerholder: string,
+    extractor: (record: MyEvent) => string
+  ) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: FilterDropdownProps) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={(node) => {
+            searchInput = node;
+          }}
+          placeholder={`Search ${searchPlacerholder}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters !== undefined ? clearFilters : () => {})}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />,
+
+    onFilter: (value: any, record: MyEvent) => extractor(record).toLocaleLowerCase().includes(value.toLowerCase()),
+
+    onFilterDropdownVisibleChange: (visible: boolean) => {
+      if (visible) {
+        setTimeout(() => searchInput.select(), 100);
+      }
+    },
+    render: (text: any) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const handleSearch = (selectedKeys: React.Key[], confirm: () => void, dataIndex: string) => {
+    confirm();
+
+    setSearchText(selectedKeys[0] as string);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText("");
+  };
 
   const cellColumns: ColumnsType<MyEvent> = [
     {
       title: <Tooltip title="Event description">Event</Tooltip>,
       key: "name",
       dataIndex: "name",
+      defaultSortOrder: "ascend",
+      sorter: (a: MyEvent, b: MyEvent) => a.name.localeCompare(b.name),
+      ...getColumnSearchProps("name", "events", (record: MyEvent) => record.name),
     },
     {
       title: "Car",
       key: "carName",
-
       dataIndex: "carName",
+      defaultSortOrder: "ascend",
+      sorter: (a: MyEvent, b: MyEvent) => a.carName.localeCompare(b.carName),
+      ...getColumnSearchProps("carName", "cars", (record: MyEvent) => record.carName),
     },
     {
       title: "Track",
       key: "trackName",
       dataIndex: "trackName",
+      defaultSortOrder: "ascend",
+      sorter: (a: MyEvent, b: MyEvent) => a.trackName.localeCompare(b.trackName),
+      ...getColumnSearchProps("trackName", "tracks", (record: MyEvent) => record.trackName),
     },
     {
       title: "Modified",
       key: "lastModified",
       dataIndex: "lastModified",
+      defaultSortOrder: "descend",
+      sorter: (a: MyEvent, b: MyEvent) => dateCompareValue(a.lastModified) - dateCompareValue(b.lastModified),
       render: (no: number, record: MyEvent) => new Date(record.lastModified!).toLocaleString(),
     },
     {
