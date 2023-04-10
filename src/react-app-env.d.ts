2f04f388ab6592aee1cd054f9181ef4f1a415aae
mpapenbr <Markus.Papenbrock+github@gmail.com>
2023-04-10 09:40:42 +0200

    Replace CRA with Vite Fixes #337

--- src/react-app-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react-scripts" />
-/* eslint-disable spaced-comment, @typescript-eslint/no-explicit-any */
-/// <reference types="react-scripts" />
-
-interface Window {
-    INITIAL_REDUX_STATE: any
-}
